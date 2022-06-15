16936bf308774831b75e9f9a8b6e4d7f39f3d1af
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-06-15 11:27:55 +0600

    revalidation added

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,5 +1,13 @@
+import { useRouter } from 'next/router'
+
 const Post = ({ post }) => {
-    console.log(post)
+
+    // Required  to build in case of fallback true
+    const router = useRouter()
+    if (router.isFallback) {
+        return <div>Loading...</div>
+    }
+
     return <div>
         <h1>Id: {post.id}</h1>
         <h4>{post.title}</h4>
@@ -12,17 +20,30 @@ const Post = ({ post }) => {
 export default Post
 
 export const getStaticPaths = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    const paths = data.map(post => {
+        return {
+            params: { postId: `${post.id}` }
+        }
+    })
     return {
-        paths: [],
-        fallback: 'blocking'
+        // paths: [],
+        // fallback: 'blocking'
+        paths,
+        fallback: true,
     }
 }
-
 export const getStaticProps = async (context) => {
     const { params } = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await response.json()
 
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
     return {
         props: {
             post: data
